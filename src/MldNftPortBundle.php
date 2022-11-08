df50ced2020b861ed7c11f756abd669c72ded73b
Geekureuil <derrey.xavier@gmail.com>
2022-11-08 12:58:03 +0100

    modif cofiguration

--- src/MldNftPortBundle.php
+++ src/MldNftPortBundle.php
@@ -2,14 +2,44 @@
 
 namespace Mld\NftPortBundle;
 
+use Symfony\Component\Config\Definition\Configurator\DefinitionConfigurator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
 class MldNftPortBundle extends AbstractBundle
 {
-    
     public function getPath(): string
     {
         return \dirname(__DIR__);
+    }
+    
+    public function configure(DefinitionConfigurator $definition): void
+    {
+        /**
+         * @formatter:off
+         */
+        $definition->rootNode()
+                    ->children()
+                        ->arrayNode('nft_port')
+                            ->children()
+                                ->scalarNode('api_key')
+                                    ->isRequired()
+                                    ->cannotBeEmpty()
+                                ->end()
+                            ->end()
+                        ->end()
+                    ->end();
+        /**
+         * @formatter:on
+         */
+    }
+    
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->services()
+                  ->get('mld_nft_port.api.nft_port_api')
+                  ->arg(0, $config['nft_port']['api_key']);
     }
     
 }
